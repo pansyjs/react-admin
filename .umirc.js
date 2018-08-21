218f5680b35630816b45d96726cf57450a00e435
wangxingkang <wangxingkang@sensoro.com>
2018-08-22 00:27:00 +0800

    feat: 封装axios

--- .umirc.js
+++ .umirc.js
@@ -12,15 +12,16 @@ export default {
     ]
   ],
   alias: {
-    '@': path.resolve(__dirname, 'src/'),
-    '@assets': path.resolve(__dirname, 'src/assets/'),
-    '@components': path.resolve(__dirname, 'src/components/'),
-    '@icons': path.resolve(__dirname, 'src/icons/'),
-    '@layouts': path.resolve(__dirname, 'src/layouts/'),
-    '@pages': path.resolve(__dirname, 'src/pages/'),
-    '@services': path.resolve(__dirname, 'src/services/'),
-    '@styles': path.resolve(__dirname, 'src/styles/'),
-    '@utils': path.resolve(__dirname, 'src/utils/'),
+    '@': path.resolve(__dirname, 'src'),
+    '@assets': path.resolve(__dirname, 'src/assets'),
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@config': path.resolve(__dirname, 'src/config'),
+    '@icons': path.resolve(__dirname, 'src/icons'),
+    '@layouts': path.resolve(__dirname, 'src/layouts'),
+    '@pages': path.resolve(__dirname, 'src/pages'),
+    '@services': path.resolve(__dirname, 'src/services'),
+    '@styles': path.resolve(__dirname, 'src/styles'),
+    '@utils': path.resolve(__dirname, 'src/utils'),
   },
   hashHistory: false
 }
