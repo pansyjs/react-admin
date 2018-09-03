1212dcbd2522f835d9ee8c0fc0819973d7cf6fca
wangxingkang <wangxingkang@sensoro.com>
2018-09-03 22:50:17 +0800

    chore: 开发环境优化

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
+const types = [
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test'
+];
+
 module.exports = {
-  extends: ['@commitlint/config-lotus']
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'subject-case': [0, 'never', ['lower-case']],
+    'type-enum': [2, 'always', types]
+  }
 };
