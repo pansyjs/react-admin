bf6cea19a80ccd18b98e4bf7a829bf4c54317a06
wangxingkang <wangxingkang@sensoro.com>
2018-09-04 10:14:38 +0800

    feat: TagSelect组件部分代码

---
+++ src/components/TagSelect/TagSelectOption.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Tag } from 'antd';
+
+const CheckableTag = Tag.CheckableTag;
+
+class TagSelectOption extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>134</div>;
+  }
+}
+
+export default TagSelectOption;
--- src/components/TagSelect/index.tsx
+++ src/components/TagSelect/index.tsx
@@ -1,28 +1,6 @@
-import React from 'react';
-import { Tag, Icon } from 'antd';
-import ClassNames from 'classnames';
-import styles from './index.scss';
+import TagSelect from './TagSelect';
+import TagSelectOption from './TagSelectOption';
 
-const CheckableTag = Tag.CheckableTag;
-
-export interface TagSelectProps {
-  value: number[] | string[];
-  defaultValue: number[] | string[];
-  onChange: (value: number[] | string[]) => any;
-  expandable: boolean;
-}
-
-class TagSelect extends React.Component<TagSelectProps, any> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expand: false
-    };
-  }
-
-  render() {
-    return <div />;
-  }
-}
+TagSelect.Option = TagSelectOption;
 
 export default TagSelect;
