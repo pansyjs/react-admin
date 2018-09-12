80fe07dd2825f17290719e692167f18d381ea19b
wangxingkang <wangxingkang@sensoro.com>
2018-09-12 10:06:00 +0800

    feat: 添加chart组件

---
+++ src/components/Charts/WaterWave/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './index.scss';
+
+class WaterWave extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>123</div>;
+  }
+}
+
+export default WaterWave;
