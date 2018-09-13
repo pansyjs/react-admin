d31f5cd7facc9adac5bafef8d0a9e3357ccf4207
wangxingkang <wangxingkang@sensoro.com>
2018-09-13 23:40:37 +0800

    feat: 封装 WaterWave 组件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { WaterWave } from '@components/Charts';
 
-class App extends React.Component {
+class IndexPage extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div>hello, 欢迎加入九毛科技。</div>;
+    return (
+      <div>
+        hello, 欢迎加入九毛科技。
+        <WaterWave height={161} title="补贴资金剩余" percent={80} />
+      </div>
+    );
   }
 }
 
-export default App;
+export default IndexPage;
