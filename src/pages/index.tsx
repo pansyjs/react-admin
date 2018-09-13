90a515aabf5f46e7e9f7108185918a089f0bf6dd
wangxingkang <wangxingkang@sensoro.com>
2018-09-13 10:31:26 +0800

    feat: 其他组件文件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import CountDown from '@components/CountDown';
+import { Tooltip, Icon, Row, Col } from 'antd';
+import { ChartCard } from '@components/Charts';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,12 +8,29 @@ class App extends React.Component {
   }
 
   render() {
-    const targetTime = new Date().getTime() + 3900000;
-
     return (
       <div>
         hello, 欢迎加入九毛科技。
-        <CountDown style={{ fontSize: 20 }} target={targetTime} />
+        <Row>
+          <Col span={4} style={{ marginTop: 24 }}>
+            <ChartCard
+              title="移动指标"
+              avatar={
+                <img
+                  style={{ width: 56, height: 56 }}
+                  src="https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png"
+                  alt="indicator"
+                />
+              }
+              action={
+                <Tooltip title="指标说明">
+                  <Icon type="info-circle-o" />
+                </Tooltip>
+              }
+              total={() => 123}
+            />
+          </Col>
+        </Row>
       </div>
     );
   }
