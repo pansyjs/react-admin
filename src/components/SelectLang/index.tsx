9aaf7470a95e5bfdc821646ba65c90f3b39486e6
wangxingkang <wangxingkang@sensoro.com>
2018-10-31 11:02:14 +0800

    feat: 多语言只支持简体中文和英语

--- src/components/SelectLang/index.tsx
+++ src/components/SelectLang/index.tsx
@@ -29,23 +29,11 @@ class SelectLang extends React.PureComponent<SelectLangProps, any> {
           </span>{' '}
           简体中文
         </Menu.Item>
-        <Menu.Item key="zh-TW">
-          <span role="img" aria-label="繁体中文">
-            🇭🇰
-          </span>{' '}
-          繁体中文
-        </Menu.Item>
         <Menu.Item key="en-US">
           <span role="img" aria-label="English">
             🇬🇧
           </span>{' '}
           English
-        </Menu.Item>
-        <Menu.Item key="pt-BR">
-          <span role="img" aria-label="Português">
-            🇵🇹
-          </span>{' '}
-          Português
         </Menu.Item>
       </Menu>
     );
