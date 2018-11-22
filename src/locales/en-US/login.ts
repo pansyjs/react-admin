05bceef4afbcb2c38815bf64cf81a2f54ddd7fc1
wangxingkang <wangxingkang@sensoro.com>
2018-11-22 23:03:11 +0800

    feat: locales js >> ts

---
+++ src/locales/en-US/login.ts
@@ -0,0 +1,10 @@
+export default {
+  'app.login.message-invalid-credentials':
+    'Invalid username or password（admin/888888）',
+  'app.login.message-invalid-verification-code': 'Invalid verification code',
+  'app.login.tab-login-credentials': 'Credentials',
+  'app.login.tab-login-mobile': 'Mobile number',
+  'app.login.sign-in-with': 'Sign in with',
+  'app.login.signup': 'Sign up',
+  'app.login.login': 'Login'
+};
---
+++ src/locales/zh-CN/login.ts
@@ -0,0 +1,9 @@
+export default {
+  'app.login.message-invalid-credentials': '账户或密码错误（admin/888888）',
+  'app.login.message-invalid-verification-code': '验证码错误',
+  'app.login.tab-login-credentials': '账户密码登录',
+  'app.login.tab-login-mobile': '手机号登录',
+  'app.login.sign-in-with': '其他登录方式',
+  'app.login.signup': '注册账户',
+  'app.login.login': '登录'
+};
