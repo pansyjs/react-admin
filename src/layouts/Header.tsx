4f2a33b37e87c56cdce1e48d4774cb288ef854e0
wangxingkang <wangxingkang@sensoro.com>
2018-12-09 00:25:24 +0800

    feat: update BasicLayout

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,17 +4,22 @@ import Animate from 'rc-animate';
 import { formatMessage } from 'umi/locale';
 import { connect } from 'dva';
 import GlobalHeader from '@/components/GlobalHeader';
+import { settingsModelState } from '@/types/settings';
 
 const { Header } = Layout;
 
 export interface HeaderProps {
-  isMobile?: boolean;
-  collapsed?: boolean;
-  fetchingNotices?: boolean;
+  logo: string;
+  isMobile: boolean;
+  collapsed: boolean;
   handleMenuCollapse?: (collapsed: boolean) => any;
+  fetchingNotices?: boolean;
+  currentUser?: any;
+  setting: settingsModelState;
 }
 
-@connect(({ global, loading }) => ({
+@connect(({ global, loading, user }) => ({
+  currentUser: user.currentUser,
   collapsed: global.collapsed,
   notices: global.notices,
   fetchingNotices: loading.effects['global/fetchNotices']
@@ -38,10 +43,28 @@ class HeaderView extends React.PureComponent<HeaderProps, any> {
   };
 
   render() {
-    const { isMobile, handleMenuCollapse } = this.props;
+    const {
+      isMobile,
+      handleMenuCollapse,
+      currentUser,
+      logo,
+      collapsed,
+      setting
+    } = this.props;
     const { visible } = this.state;
 
-    const HeaderDom = visible ? <Header>123</Header> : null;
+    const HeaderDom = visible ? (
+      <Header style={{ padding: 0 }}>
+        <GlobalHeader
+          logo={logo}
+          collapsed={collapsed}
+          onCollapse={handleMenuCollapse}
+          isMobile={isMobile}
+          currentUser={currentUser}
+        />
+      </Header>
+    ) : null;
+
     return (
       <Animate component="" transitionName="fade">
         {HeaderDom}
