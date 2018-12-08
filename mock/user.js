b8e2439351984b08816efeee11538f38cfe88075
wangxingkang <wangxingkang@sensoro.com>
2018-12-09 00:38:08 +0800

    feat: update get userinfo

--- mock/user.js
+++ mock/user.js
@@ -1,55 +1,13 @@
 function getCurrentUser(req, res) {
-  const { token } = req.headers;
-  // token不存在
-  if (!token) {
-    res.json({
-      status: 50008,
-      data: {},
-      message: '非法的token'
-    });
-    res.status(200).end();
-    return;
-  }
-  const role = token.split('_')[0];
-  // 管理员
-  if (role === 'admin') {
-    res.json({
-      status: 200,
-      data: {
-        id: 'a81n02nw43qp4wq112',
-        username: 'admin',
-        role: {
-
-        }
-      },
-      message: 'success'
-    });
-    res.status(200).end();
-    return;
-  }
-
-  // 普通用户
-  if (role === 'user') {
-    res.json({
-      status: 200,
-      data: {
-        id: 'a81n02nw43qp4wq112',
-        username: 'admin',
-        role: {
-
-        }
-      },
-      message: 'success'
-    });
-    res.status(200).end()
-  } else {
-    res.json({
-      status: 50008,
-      data: {},
-      message: '非法的token'
-    });
-    res.status(200).end();
-  }
+  res.json({
+    status: 200,
+    data: {
+      name: 'Tom',
+      avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
+    },
+    message: 'success'
+  });
+  res.status(200).end();
 }
 
 function login(req, res) {
--- src/locales/en-US/menu.ts
+++ src/locales/en-US/menu.ts
@@ -3,5 +3,10 @@ export default {
   'menu.dashboard': 'Dashboard',
   'menu.dashboard.analysis': 'Analysis',
   'menu.dashboard.monitor': 'Monitor',
-  'menu.dashboard.workplace': 'Workplace'
+  'menu.dashboard.workplace': 'Workplace',
+  'menu.account': 'Account',
+  'menu.account.center': 'Account Center',
+  'menu.account.settings': 'Account Settings',
+  'menu.account.trigger': 'Trigger Error',
+  'menu.account.logout': 'Logout'
 };
