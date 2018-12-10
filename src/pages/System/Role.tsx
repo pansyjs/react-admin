b0706b791c6dadc845654c663881468b98fcc2b3
wangxingkang <wangxingkang@sensoro.com>
2018-12-10 22:17:31 +0800

    feat: add System module

---
+++ src/pages/System/Role.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from '@/components/BaseComponent';
+
+export interface SystemRoleProps {}
+
+class SystemRole extends Component<SystemRoleProps, any> {
+  render() {
+    return <div>SystemRole</div>;
+  }
+}
+
+export default SystemRole;
