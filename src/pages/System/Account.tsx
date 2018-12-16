54c45515c190a4ea1bb14da48b413340219838d1
wangxingkang <wangxingkang@sensoro.com>
2018-12-16 14:02:45 +0800

    feat: update system module

---
+++ src/pages/System/Account.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from '@/components/BaseComponent';
+
+export interface SystemRoleProps {}
+
+class Account extends Component<SystemRoleProps, any> {
+  render() {
+    return <div>Account</div>;
+  }
+}
+
+export default Account;
