c6f7d238938eb1a7cc971378be4eabf827ee2fd1
wangxingkang <wangxingkang@sensoro.com>
2019-04-07 23:20:46 +0800

    fix: 修复报错

--- src/components/global-header/right-content.tsx
+++ src/components/global-header/right-content.tsx
@@ -5,7 +5,7 @@ import { Spin, Tag, Menu, Icon, Avatar, Tooltip, message } from 'antd';
 import { ClickParam } from 'antd/es/menu';
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import HeaderDropdown from '@/components/header-dropdown';
-import { SelectLang } from '@/components/select-lang';
+import SelectLang from '@/components/select-lang';
 import { ICurrentUser } from '@/models/user';
 
 interface IProps {
