ed8cbcde8d0c8f4945d74acbcaf12f7bfd4d3377
wangxingkang <wangxingkang@sensoro.com>
2019-04-08 11:26:53 +0800

    feat: add exception module

---
+++ src/pages/authorized.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'dva';
+import pathToRegexp from 'path-to-regexp';
+
+interface IProps {
+
+}
+
+const AuthComponent: React.FC<IProps> = (props) => {
+  const { children } = props;
+  console.log(props);
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default connect(({ menu }) => ({
+  routerData: menu.routerData,
+}))(AuthComponent);
+
