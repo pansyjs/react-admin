777be97b1fa42cc26b177e268166d850a25cbf16
wangxingkang <wangxingkang@sensoro.com>
2019-06-22 11:15:47 +0800

    feat: 整理开发依赖，添加图表示例

--- src/pages/dashboard/analysis.tsx
+++ src/pages/dashboard/analysis.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Pie } from 'rc-charts';
 
-class AnalysisPage extends React.Component {
-  render() {
-    return <div>Analysis</div>;
-  }
-}
+const AnalysisPage: React.FC = () => {
+  return (
+    <div>
+      <Pie
+        data={[{ x: 'chrome', y: 20 }, { x: 'IE', y: 20 }, { x: 'Firefox', y: 20 }]}
+        tooltip={true}
+        subTitle="总预警数"
+        total={100}
+      />
+    </div>
+  );
+};
 
 export default AnalysisPage;
