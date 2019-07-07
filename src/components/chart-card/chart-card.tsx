1ccdba4f4c744057386b1fb71c35e1c00b5f2481
wangxingkang <wangxingkang@sensoro.com>
2019-07-07 13:23:01 +0800

    chore: 开发依赖整理

---
+++ src/components/chart-card/chart-card.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Card } from 'antd';
+import { CardProps } from 'antd/es/card';
+
+const ChartCard: React.FC = () => {
+  return <Card></Card>;
+};
+
+export default ChartCard;
