a835c4c0160b802a3ff2827ce6d9d8377ac8a7fc
wangxingkang <156148958@qq.com>
2020-09-10 21:55:51 +0800

    feat(login): update layout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,6 +6,7 @@ import {
 } from '@ant-design/icons';
 import { Alert, Checkbox } from 'antd';
 import { Link } from 'umi';
+import UserLayout from '@/layouts/user-layout';
 import { LoginParamsType } from '@/common/types/login';
 import LoginForm from '@/components/login';
 import styles from './style.less';
@@ -48,87 +49,89 @@ const Login: React.FC<LoginProps> = (props) => {
   };
 
   return (
-    <div className={styles.main}>
-      <LoginForm activeKey={type} onTabChange={setType} onSubmit={handleSubmit}>
-        <Tab key="account" tab="账户密码登录">
-          {status === 'error' && loginType === 'account' && !submitting && (
-            <LoginMessage content="账户或密码错误（admin/ant.design）" />
-          )}
+    <UserLayout>
+      <div className={styles.main}>
+        <LoginForm activeKey={type} onTabChange={setType} onSubmit={handleSubmit}>
+          <Tab key="account" tab="账户密码登录">
+            {status === 'error' && loginType === 'account' && !submitting && (
+              <LoginMessage content="账户或密码错误（admin/ant.design）" />
+            )}
 
-          <UserName
-            name="userName"
-            placeholder="用户名: admin or user"
-            rules={[
-              {
-                required: true,
-                message: '请输入用户名!',
-              },
-            ]}
-          />
-          <Password
-            name="password"
-            placeholder="密码: ant.design"
-            rules={[
-              {
-                required: true,
-                message: '请输入密码！',
-              },
-            ]}
-          />
-        </Tab>
-        <Tab key="mobile" tab="手机号登录">
-          {status === 'error' && loginType === 'mobile' && !submitting && (
-            <LoginMessage content="验证码错误" />
-          )}
-          <Mobile
-            name="mobile"
-            placeholder="手机号"
-            rules={[
-              {
-                required: true,
-                message: '请输入手机号！',
-              },
-              {
-                pattern: /^1\d{10}$/,
-                message: '手机号格式错误！',
-              },
-            ]}
-          />
-          <Captcha
-            name="captcha"
-            placeholder="验证码"
-            rules={[
-              {
-                required: true,
-                message: '请输入验证码！',
-              },
-            ]}
-          />
-        </Tab>
-        <div>
-          <Checkbox checked={autoLogin} onChange={(e) => setAutoLogin(e.target.checked)}>
-            自动登录
-          </Checkbox>
-          <a
-            style={{
-              float: 'right',
-            }}
-          >
-            忘记密码
-          </a>
-        </div>
-        <Submit loading={submitting}>登录</Submit>
-        <div className={styles.other}>
-          其他登录方式
-          <AlipayCircleOutlined className={styles.icon} />
-          <TaobaoCircleOutlined className={styles.icon} />
-          <WeiboCircleOutlined className={styles.icon} />
-          <Link className={styles.register} to="/user/register">
-            注册账户
-          </Link>
-        </div>
-      </LoginForm>
-    </div>
+            <UserName
+              name="userName"
+              placeholder="用户名: admin or user"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入用户名!',
+                },
+              ]}
+            />
+            <Password
+              name="password"
+              placeholder="密码: ant.design"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入密码！',
+                },
+              ]}
+            />
+          </Tab>
+          <Tab key="mobile" tab="手机号登录">
+            {status === 'error' && loginType === 'mobile' && !submitting && (
+              <LoginMessage content="验证码错误" />
+            )}
+            <Mobile
+              name="mobile"
+              placeholder="手机号"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入手机号！',
+                },
+                {
+                  pattern: /^1\d{10}$/,
+                  message: '手机号格式错误！',
+                },
+              ]}
+            />
+            <Captcha
+              name="captcha"
+              placeholder="验证码"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入验证码！',
+                },
+              ]}
+            />
+          </Tab>
+          <div>
+            <Checkbox checked={autoLogin} onChange={(e) => setAutoLogin(e.target.checked)}>
+              自动登录
+            </Checkbox>
+            <a
+              style={{
+                float: 'right',
+              }}
+            >
+              忘记密码
+            </a>
+          </div>
+          <Submit loading={submitting}>登录</Submit>
+          <div className={styles.other}>
+            其他登录方式
+            <AlipayCircleOutlined className={styles.icon} />
+            <TaobaoCircleOutlined className={styles.icon} />
+            <WeiboCircleOutlined className={styles.icon} />
+            <Link className={styles.register} to="/user/register">
+              注册账户
+            </Link>
+          </div>
+        </LoginForm>
+      </div>
+    </UserLayout>
   );
 };
 
