ba80c8c201eaf18d438dbf6ca944cd505ac99a8c
wangxingkang <156148958@qq.com>
2020-09-10 21:49:07 +0800

    feat(components): add HeaderDropdown component

--- src/components/header-dropdown/index.tsx
+++ src/components/header-dropdown/index.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react';
-import classNames from 'classnames';
-import { Dropdown } from 'antd';
 import { DropDownProps } from 'antd/es/dropdown';
+import { Dropdown } from 'antd';
+import React from 'react';
+import classNames from 'classnames';
 import styles from './index.less';
 
 declare type OverlayFunc = () => React.ReactNode;
@@ -12,7 +12,7 @@ export interface HeaderDropdownProps extends Omit<DropDownProps, 'overlay'> {
   placement?: 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topCenter' | 'topRight' | 'bottomCenter';
 }
 
-const HeaderDropdown: FC<HeaderDropdownProps> = ({ overlayClassName: cls, ...restProps }) => (
+const HeaderDropdown: React.FC<HeaderDropdownProps> = ({ overlayClassName: cls, ...restProps }) => (
   <Dropdown overlayClassName={classNames(styles.container, cls)} {...restProps} />
 );
 
