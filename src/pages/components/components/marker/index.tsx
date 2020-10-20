c4f43ade382ae9ccdd533dd94b933cb2e65b1288
wangxingkang <156148958@qq.com>
2020-10-20 21:50:05 +0800

    feat(map): 地图优化

--- src/pages/components/components/marker/index.tsx
+++ src/pages/components/components/marker/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import LockFilled from '@ant-design/icons/LockFilled';
+import ShopOutlined from '@ant-design/icons/ShopOutlined';
+
 import styles from './index.less';
 
 // 聚合标记点
 const Marker: React.FC = () => {
   return (
     <div className={styles.marker}>
-      <LockFilled />
+      <ShopOutlined />
     </div>
   );
 };
--- src/pages/components/map.tsx
+++ src/pages/components/map.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Map, Markers } from '@pansy/react-amap';
+import { Map, Markers, ToolBar } from '@pansy/react-amap';
 import { Marker } from './components';
 import './map.less';
 
@@ -20,6 +20,7 @@ const MapComponent: React.FC = () => {
     <PageContainer>
       <div style={{ width: '100%', height: 800 }}>
         <Map zoom={5}>
+          <ToolBar liteStyle offset={[10, 95]} />
           <Markers
             markers={markers}
             useCluster={{
@@ -27,11 +28,7 @@ const MapComponent: React.FC = () => {
                 return marker.setContent(`<div class="map-markers">${count}</div>`);
               }
             }}
-            render={() => {
-              return (
-                <Marker />
-              )
-            }}
+            render={() => <Marker />}
           />
         </Map>
       </div>
