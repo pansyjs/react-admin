5899d926b69e591671b2439fea833eff01e0710e
wangxingkang <wang_xingkang@qq.com>
2022-04-07 23:30:24 +0800

    ✨ feat: update watermark page

--- src/pages/libraries/watermark/index.tsx
+++ src/pages/libraries/watermark/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
 import { Watermark } from '@pansy/react-watermark';
 
 export default () => {
   return (
-    <Watermark isBody text={'刘德华 7878'} />
+    <PageContainer>
+      <div>
+        <Watermark isBody text={'刘德华 7878'} />
+          123
+      </div>
+    </PageContainer>
   )
 }
